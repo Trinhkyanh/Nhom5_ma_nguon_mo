a63ed249c9fae786a31fa9eb8ee3438dd232f5c7
Trinhkyanh <145025221+Trinhkyanh@users.noreply.github.com>
2023-09-26 19:08:44 +0700

    Add files via upload

---
+++ thong_ke.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from numpy import array
+import matplotlib.pyplot as plt
+import numpy as np
+df=pd.read_csv('diemPython.csv',index_col=0,header = 0)
+in_data = array(df.iloc[:,:])
+print(in_data)
+print('Tong so sinh vien di thi :')
+tongsv= in_data[:,1]
+print(np.sum(tongsv))
+diemA = in_data[:,3]
+diemBc = in_data[:,4]
+print('Tong sv:',tongsv)
+maxa = diemA.max()
+i, = np.where(diemA == maxa)
+print('lop co nhieu diem A la {0} co {1} sv dat diem A'.format(in_data[i,0],maxa))
+plt.plot(range(len(diemA)),diemA,'r-',label="Diem A")
+plt.plot(range(len(diemBc)),diemBc,'g-',label="Diem B +")
+plt.xlabel('Lơp')
+plt.ylabel(' So sv dat diem ')
+plt.legend(loc='upper right')
+plt.show()
+      
